7c0e562b33cd04461d5804ff2e8c0ad9d83cdfbc
Jim Hokanson <jim.hokanson@gmail.com>
2016-08-13 15:07:06 -0400

    documentation updates

---
+++ pubmed/journals.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+List of journals:
+Multiple links to the list : http://www.ncbi.nlm.nih.gov/books/NBK3828/#publisherhelp.How_do_I_know_if_my_journa
+https://www.nlm.nih.gov/bsd/serfile_addedinfo.html
+Link to an uncompressed vesion - ftp://ftp.ncbi.nih.gov/pubmed/J_Medline.txt
+
+Package Features
+----------------
+1) Provide search help for mathching journals
+   - download list if not available locally
+   - store as binary for quick load
+   - provide search mechanism
+2) Allow tracking of a journal's history (old names)
+    Journal X became Y became Z
+3) Journal to publisher
+
+
+
+"""
+
+
+
